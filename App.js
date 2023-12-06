6d41e6ab4b799b39efe6cb054d52d0999ca1903a
Abdurrahim-10 <abdul.rahim@armadainfotech.in>
2023-12-06 14:33:48 +0530

    ss

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text style={{fontSize: 20, color: 'black', fontWeight: 'bold'}}>
+        India
+      </Text>
+    </View>
+  );
+}
+
+//t
